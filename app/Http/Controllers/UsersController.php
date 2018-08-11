32ac0a739506aeb0da58a1832fa7fc51a82e4099
vagrant <vagrant@homestead>
2018-08-11 04:03:47 +0000

    Finish login and logout

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -32,6 +32,7 @@ class UsersController extends Controller
             'password' => bcrypt($request->password),
         ]);
 
+        Auth::login($user);    #用户注册成功后自动登录
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
     }
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -10,7 +10,8 @@
     <div class="panel-body">
         @include('shared._errors')
       <form method="POST" action="{{ route('users.store') }}">
-         {{ csrf_field() }}
+        {{ csrf_field() }}
+
           <div class="form-group">
             <label for="name">名称：</label>
             <input type="text" name="name" class="form-control" value="{{ old('name') }}">
